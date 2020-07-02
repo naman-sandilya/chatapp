74ad3533429295f2bebd7aa541fba6f7204b992e
Naman Sandilya <namansandilya@gmail.com>
2020-07-02 15:34:47 +0530

    Setting up Client and Server

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// Local Imports
+
+// Package Imports
+
+const App = () => {
+  return <div>This is from App</div>;
+};
+
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Local Imports
+import App from './App';
+
+// Package Imports
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
